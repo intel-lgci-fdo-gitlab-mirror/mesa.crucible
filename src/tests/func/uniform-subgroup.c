6f40e708b34570124cd8c8986f1a313f1ec4f4d8
Jason Ekstrand <jason.ekstrand@collabora.com>
2022-06-23 11:41:08 -0500

    func.uniform-subgroup: Set VkComputePipelineCreateInfo::stage::sType

--- src/tests/func/uniform-subgroup.c
+++ src/tests/func/uniform-subgroup.c
@@ -62,6 +62,7 @@ common_init(VkShaderModule cs, const uint32_t ssbo_size,
             .sType = VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO,
             .pNext = NULL,
             .stage = {
+                .sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
                 .stage = VK_SHADER_STAGE_COMPUTE_BIT,
                 .module = cs,
                 .pName = "main",
